61c3d9d59775d243c56b339ab45c7c6c937915ba
Jin <jin.lai.au@gmail.com>
2023-05-06 22:46:55 +1000

    update

---
+++ problem.py
@@ -0,0 +1,40 @@
+#####################################################################################
+#  Script that ingests data from YAML or JSON variables and renders the configuration
+#  with a configuration template. Example
+#
+#  python configurator.py interface_vars.yaml cisco_intf_template.j2
+#####################################################################################
+import sys
+import os
+
+# use of range() to define a range of values
+digits = [1, 2, 3, 4]
+
+# iterate from i = 0 to i = 3
+for c in digits:
+    if c==2 or c==3:
+        continue
+
+    for l in digits:
+
+#        if l==2 or l==4 or l==c:
+        if l==2 or l!=4 or l==c:
+            continue
+
+        for m in digits:
+            if m==c or m==l:
+                continue
+
+            for n in digits:
+                if n==c or n==l or n==m :
+                    continue
+                elif n==1 or n==3:
+                    continue
+                else:
+                    print("c=", c)
+                    print("l=", l)
+                    print("m=", m)
+                    print("n=", n)
+                    print("------------------------------")
+
+
